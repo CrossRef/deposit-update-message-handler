75ad4e409788b7d5abd2df950483ebcaa92bc7e2
Joe Wass <joe@afandian.com>
2013-12-06 21:57:31 +0000

    Simply xml parsing

--- src/deposit_update_message_handler/parse.clj
+++ src/deposit_update_message_handler/parse.clj
@@ -23,14 +23,10 @@
 (defn extract-record-info 
   "Extract information from a record_diagnostic tag."
   [record]
-  ; (prn "RECORD")
-  
-  
-  ; (prn (z/xml1-> record :attrs :status))
   { 
    :success (= (-> (first record) :attrs :status) "Success")
-   :doi (first (z/xml-> record :doi z/text))
-   :message (trim (first (z/xml-> record :msg z/text)))
+   :doi (z/xml1-> record :doi z/text)
+   :message (trim (z/xml1-> record :msg z/text))
     }
   )
 
@@ -40,21 +36,15 @@
   (let [
         x (xml/parse (java.io.ByteArrayInputStream. (.getBytes input)))
         zipped (zip/xml-zip x)
-        ; TODO firsts intl ->xml1
-        submission-id (parse-int-or-zero (first (z/xml-> zipped :submission_id z/text)))
-        batch-id (parse-int-or-zero (first (z/xml-> zipped :batch_id z/text)))
-        record-count (parse-int-or-zero(first (z/xml-> zipped :batch_data :record_count z/text)))
-        success-count (parse-int-or-zero(first (z/xml-> zipped :batch_data :success_count z/text)))
-        warning-count (parse-int-or-zero(first (z/xml-> zipped :batch_data :warning_count z/text)))
-        failure-count (parse-int-or-zero(first (z/xml-> zipped :batch_data :failure_count z/text)))
+        submission-id (parse-int-or-zero (z/xml1-> zipped :submission_id z/text))
+        batch-id (parse-int-or-zero (z/xml1-> zipped :batch_id z/text))
+        record-count (parse-int-or-zero(z/xml1-> zipped :batch_data :record_count z/text))
+        success-count (parse-int-or-zero(z/xml1-> zipped :batch_data :success_count z/text))
+        warning-count (parse-int-or-zero(z/xml1-> zipped :batch_data :warning_count z/text))
+        failure-count (parse-int-or-zero(z/xml1-> zipped :batch_data :failure_count z/text))
         records (z/xml-> zipped :record_diagnostic)
         records-info (map extract-record-info records)
     ]
-    
-    ; (prn "RECORDS")
-    ; (prn records)
-    ; (prn "INFO")
-    ; (prn records-info)
         
     {
      :submission-id submission-id
